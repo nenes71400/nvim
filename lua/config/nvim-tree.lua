a2b8a1bdc711e2d987216f43ef699350e62d4cb1
Alexis Desvignes <a.desvignes@aeb-autun.com>
2022-03-31 08:28:32 +0200

    Ajout Langage server lsp

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,15 +1,15 @@
 local g = vim.g
 
-g.nvim_tree_indent_markers = 1 -- 0 by default, this option shows indent markers when folders are open
-g.nvim_tree_git_hl = 1 -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
-g.nvim_tree_highlight_opened_files = 1 -- 0 by default, will enable folder and file icon highlight for opened files/directories.
+g.nvim_tree_indent_markers = 1          -- 0 by default, this option shows indent markers when folders are open
+g.nvim_tree_git_hl = 1                  -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
+g.nvim_tree_highlight_opened_files = 1  -- 0 by default, will enable folder and file icon highlight for opened files/directories.
 g.nvim_tree_root_folder_modifier = ":~" -- This is the default. See :help filename-modifiers for more options
-g.nvim_tree_add_trailing = 1 -- 0 by default, append a trailing slash to folder names
-g.nvim_tree_group_empty = 1 --  0 by default, compact folders that only contain a single folder into one node in the file tree
-g.nvim_tree_disable_window_picker = 1 -- 0 by default, will disable the window picker.
-g.nvim_tree_icon_padding = " " -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
-g.nvim_tree_symlink_arrow = " >> " --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
-g.nvim_tree_respect_buf_cwd = 1 -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
+g.nvim_tree_add_trailing = 1            -- 0 by default, append a trailing slash to folder names
+g.nvim_tree_group_empty = 1             --  0 by default, compact folders that only contain a single folder into one node in the file tree
+-- g.nvim_tree_disable_window_picker = 1   -- 0 by default, will disable the window picker.
+g.nvim_tree_icon_padding = " "          -- one space by default, used for rendering the space between the icon and the filename. Use with caution, it could break rendering if you set an empty string depending on your font.
+g.nvim_tree_symlink_arrow = " >> "      --  defaults to ' ➛ '. used as a separator between symlinks' source and target.
+g.nvim_tree_respect_buf_cwd = 1         -- 0 by default, will change cwd of nvim-tree to that of new buffer's when opening nvim-tree.
 
 g.nvim_tree_show_icons = {git = 1, folders = 1, files = 1, folder_arrows = 1}
 g.nvim_tree_icons = {
@@ -43,7 +43,7 @@ require("nvim-tree").setup {
     -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup = {},
     -- closes neovim automatically when the tree is the last **WINDOW** in the view
-    auto_close = true,
+    -- auto_close = true,
     -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab = true,
     -- hijack the cursor in the tree to put it at the start of the filename
